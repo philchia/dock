2a77bccfd4fe3275344b64bdf706431a2ae34c8a
Phil <zgrubby@gmail.com>
2018-11-28 14:03:52 +0800

    rename to container id

--- internal/subsystem/cgroup.go
+++ internal/subsystem/cgroup.go
@@ -6,20 +6,20 @@ import (
 
 // CgroupManager manage all subsystems
 type CgroupManager struct {
-	cgroup string
+	containerID string
 }
 
 // NewCgroupManager create new CgroupManager
-func NewCgroupManager(cgroup string) *CgroupManager {
+func NewCgroupManager(containerID string) *CgroupManager {
 	return &CgroupManager{
-		cgroup: cgroup,
+		containerID: containerID,
 	}
 }
 
 // Set resource limit for subsystems
 func (m *CgroupManager) Set(conf *ResourceConfig) error {
 	for _, subsystem := range systems {
-		if err := subsystem.Set(m.cgroup, conf); err != nil {
+		if err := subsystem.Set(m.containerID, conf); err != nil {
 			return err
 		}
 	}
@@ -30,7 +30,7 @@ func (m *CgroupManager) Set(conf *ResourceConfig) error {
 // Apply add process's pid to all sub systems
 func (m *CgroupManager) Apply(pid int) error {
 	for _, subsystem := range systems {
-		if err := subsystem.Apply(m.cgroup, pid); err != nil {
+		if err := subsystem.Apply(m.containerID, pid); err != nil {
 			return err
 		}
 	}
@@ -41,7 +41,7 @@ func (m *CgroupManager) Apply(pid int) error {
 // Destroy remove all subsystems
 func (m *CgroupManager) Destroy() error {
 	for _, subsystem := range systems {
-		if err := subsystem.Remove(m.cgroup); err != nil {
+		if err := subsystem.Remove(m.containerID); err != nil {
 			log.Println("[ERROR]:", err)
 		}
 	}
